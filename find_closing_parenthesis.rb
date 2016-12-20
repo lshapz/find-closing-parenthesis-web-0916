95633f4ef937984144dde2085f044ac457e7c734
lshapz <laurashapiro01@gmail.com>
2016-12-20 13:38:18 -0500

    Done.

--- find_closing_parenthesis.rb
+++ find_closing_parenthesis.rb
@@ -1,3 +1,21 @@
 def find_closing_parenthesis(sentence, open_position)
 
+  split = sentence.split('') #.slice(open_position, sentence.length)
+
+  search_string = split[open_position, sentence.length]
+  openers = 0
+  starter = open_position
+
+  search_string.each_with_index do |x, ind| 
+    if x == "(" 
+      openers += 1
+    elsif x == ")"
+      openers -= 1
+      if openers == 0 
+        return ind + starter
+      end
+    end
+  end
+
+
 end
